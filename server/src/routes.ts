01d75b12a98b131f25ded3324a113d2a32635d2f
gabrielcancio <gabriel.cancio02@gmail.com>
2020-06-04 16:39:25 -0300

    Fixing item get route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
 import PointsController from './controllers/PointsCotroller';
-import ItemsController from './controllers/PointsCotroller';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/items', itemsController.create);
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
