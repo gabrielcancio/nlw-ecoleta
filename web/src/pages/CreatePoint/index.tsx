1df2a164b1a8237ada2fe5871fba881c583dacc6
gabrielcancio <gabriel.cancio02@gmail.com>
2020-06-05 12:30:58 -0300

    Making connection with API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,164 @@
-import React from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import { Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi';
+import axios from 'axios';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
+interface IItemResponse {
+    id: number;
+    title: string,
+    image_url: string
+}
+
+interface IIBGEUFResponse {
+    sigla: string;
+}
+
+interface IIBGECityResponse {
+    nome: string;
+}
+
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<IItemResponse[]>([]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        whatsapp: ''
+    });
+
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [selectedUf, setSelectedUf] = useState('0');
+
+    const [cities, setCities] = useState<string[]>([]);
+    const [selectedCity, setSelectedCity] = useState('0');
+
+    const [userPosition, setUserPosition] = useState<[number, number]>([0,0]);
+    const [selectedPostion, setSelectedPostion] = useState<[number, number]>(userPosition);
+
+    useEffect(() => {
+        api.get<IItemResponse[]>('items').then(response => setItems(response.data));
+    }, []);
+
+    useEffect(() => {
+        axios.get<IIBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            const ufInitials = response.data.map(uf => uf.sigla);
+
+            setUfs(ufInitials);
+        });
+    }, []);
+
+    useEffect(() => {
+        if(selectedUf === '0') {
+            return;
+        }
+
+        axios.get<IIBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+            const cityNames = response.data.map(city => city.nome);
+
+            setCities(cityNames);
+            
+        });
+    }, [selectedUf]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+
+            setUserPosition([
+                latitude,
+                longitude
+            ]);
+        });
+    }, []);
+
+   function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
+       const uf = event.target.value;
+
+       setSelectedUf(uf);
+   }
+
+   function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
+       const city = event.target.value;
+
+       setSelectedCity(city);
+   }
+
+   function handleMapClick(event: LeafletMouseEvent) {
+        setSelectedPostion([
+            event.latlng.lat,
+            event.latlng.lng
+        ]);
+   }
+   
+   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        const { name, value } = event.target;
+
+        setFormData({ ...formData,  [name]: value});
+   }
+
+   function handleSelectItem(id: number) {
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if(alreadySelected >= 0) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems);
+        } else {
+            setSelectedItems([...selectedItems, id]);
+        }
+
+        /**
+         * Ou:
+         * 
+         * if(selectedItems.includes(id)) {
+            return setSelectedItems(selectedItems.filter(ids => ids !== id));
+        }
+
+        setSelectedItems([...selectedItems,id])
+         */
+   }
+
+   async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        const { name, email, whatsapp } = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPostion;
+        const items = selectedItems;
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        }
+
+        try {
+            await api.post('points', data);
+
+            alert('Ponto de coleta cadastrado com sucesso.')
+        } catch(err) {
+            alert('Erro ao cadastrar ponto de coleta. Tente novamente')
+
+            console.log(err);
+        }
+
+   }
+
     return(
         <div id="page-create-point">
             <header>
@@ -19,7 +170,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/>ponto de coleta</h1>
 
                 <fieldset>
@@ -33,6 +184,8 @@ const CreatePoint = () => {
                           type="text"
                           name="name"
                           id="name"
+                          onChange={handleInputChange}
+                          required
                         />
                     </div>
 
@@ -43,6 +196,8 @@ const CreatePoint = () => {
                             type="text"
                             name="email"
                             id="email"
+                            onChange={handleInputChange}
+                            required
                             />
                         </div>
 
@@ -52,17 +207,19 @@ const CreatePoint = () => {
                             type="text"
                             name="whatsapp"
                             id="whatsapp"
+                            onChange={handleInputChange}
+                            required
                             /> 
                         </div>
                     </div>
                 </fieldset>
 
-                <Map center={[-2.5343526, -44.1625244]} zoom={15}>
+                <Map center={userPosition} zoom={15} onClick={handleMapClick}>
                     <TileLayer 
                       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
                     />
-                    <Marker position={[-2.5343526, -44.1625244]}/>
+                    <Marker position={selectedPostion}/>
                 </Map>
 
                 <fieldset>
@@ -73,14 +230,32 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select 
+                              name="uf" 
+                              id="uf" 
+                              onChange={handleSelectedUf} 
+                              value={selectedUf}
+                              required
+                            >
                                 <option value="0">Selecione uma UF</option>
+                                  {ufs.map(uf => (
+                                <option key={uf} value={uf}>{uf}</option>
+                                  ))}
                             </select>
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select 
+                              name="city" 
+                              id="city" 
+                              onChange={handleSelectedCity} 
+                              value={selectedCity}
+                              required
+                            >
                                 <option value="0">Selecione uma cidade</option>
+                                  {cities.map(city => (
+                                <option key={city} value={city}>{city}</option>
+                                  ))}
                             </select>
                         </div>
                     </div>
@@ -92,30 +267,16 @@ const CreatePoint = () => {
                         <span>Selecione um ou mais itens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Resíduos eletrônicos"/>
-                            <span>Resíduos Eletrônicos</span>
+                       {items.map(item => (
+                        <li 
+                          key={item.id} 
+                          onClick={() => handleSelectItem(item.id)}
+                          className={selectedItems.includes(item.id) ? 'selected' : '' }
+                        >
+                            <img src={item.image_url} alt={item.title}/>
+                            <span> {item.title} </span>
                         </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Resíduos eletrônicos"/>
-                            <span>Resíduos Eletrônicos</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Resíduos eletrônicos"/>
-                            <span>Resíduos Eletrônicos</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Resíduos eletrônicos"/>
-                            <span>Resíduos Eletrônicos</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Resíduos eletrônicos"/>
-                            <span>Resíduos Eletrônicos</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Resíduos eletrônicos"/>
-                            <span>Resíduos Eletrônicos</span>
-                        </li>
+                       ))}
                     </ul>
                 </fieldset>
 
